a6d4f7a0623a7d895222c19f048bae9216ab0147
Axel Elmarsson <axel.elmarsson@gmail.com>
2022-05-05 13:20:03 +0200

    feat: rework frontend

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,32 @@ import Shorten from "./views/Shorten";
 import RedirectComponent from "./views/RedirectComponent";
 import Links from "./views/Links";
 import useAuthorization from "./hooks/useAuthorization";
+import axios from "axios";
+import Configuration from "./configuration";
+import { MantineProvider } from '@mantine/core';
+
+axios.defaults.baseURL = Configuration.apiUrl;
+axios.defaults.headers.post["Content-Type"] = "application/json"
+axios.interceptors.request.use(
+    config => {
+        if (!config.headers.Authorization) {
+            const token = localStorage.getItem("token");
+            if (token) config.headers.Authorization = `Bearer ${token}`
+        }
+
+        return config;
+    },
+    error => Promise.reject(error),
+)
 
 const defaultLinks = [
     <Link to="/shorten" key="methonl-1">Förkorta</Link>,
 ]
 
-export const LoggedInContext = React.createContext({ loading: true, hasToken: false })
-
 const App = () => {
 
     const [methoneLinks, setMethoneLinks] = useState(defaultLinks)
-    const { pls, loading, hasToken } = useAuthorization();
+    const { pls, loading, hasToken, user } = useAuthorization();
 
     useEffect(() => {
         if (hasToken) {
@@ -24,9 +39,38 @@ const App = () => {
         } else setMethoneLinks(defaultLinks)
     }, [pls, loading])
 
+    const [userMandates, setUserMandates] = useState([])
+    const [allMandates, setallMandates] = useState([])
+
+    const getUserMandates = () => {
+        if (user.length === 0) return;
+        axios.get(`https://dfunkt.datasektionen.se/api/user/kthid/${user}/current`, { headers: { Authorization: "" } })
+            .then(res => {
+                setUserMandates(res.data.mandates);
+            })
+            .catch(console.log)
+    }
+
+    const getAllMandates = () => {
+        axios.get("https://dfunkt.datasektionen.se/api/roles", { headers: { Authorization: "" } })
+            .then(res => {
+                setallMandates(res.data);
+            })
+            .catch(console.log)
+    }
+
+    useEffect(getUserMandates, [user])
+    useEffect(getAllMandates, [])
+
     return (
-        <div id="application" className="light-blue">
-            <LoggedInContext.Provider value={{ loading, hasToken }}>
+        <MantineProvider
+            theme={{
+                fontFamily: "Lato",
+                headings: { fontFamily: "Lato" },
+                primaryColor: "blue"
+            }}
+        >
+            <div id="application" className="light-blue">
                 <Methone config={{
                     system_name: "link-shortener",
                     login_href: hasToken ? "/logout" : "/login",
@@ -37,30 +81,30 @@ const App = () => {
                 />
                 <Switch>
                     <Route exact path="/shorten">
-                        <Shorten />
+                        <Shorten userMandates={userMandates} />
                     </Route>
                     <Route exact path="/links">
-                        <Links />
+                        <Links user={user} userMandates={userMandates} allMandates={allMandates} pls={pls} />
                     </Route>
                     <Route exact path="/login" render={match => {
-                            window.location = `https://login.datasektionen.se/login?callback=${encodeURIComponent(window.location.origin)}/token/`;
-                            return <div></div>
-                        }} />
-                        <Route exact path="/logout" render={({ match }) => {
-                            localStorage.removeItem('token')
-                            window.location = "/shorten";
-                            return <div></div>
-                        }} />
-                        <Route exact path="/token/:token" render={({ match, history }) => {
-                            localStorage.setItem('token', match.params.token);
-                            return <Redirect to="/shorten" />
-                        }} />
+                        window.location = `https://login.datasektionen.se/login?callback=${encodeURIComponent(window.location.origin)}/token/`;
+                        return <div></div>
+                    }} />
+                    <Route exact path="/logout" render={({ match }) => {
+                        localStorage.removeItem('token')
+                        window.location = "/shorten";
+                        return <div></div>
+                    }} />
+                    <Route exact path="/token/:token" render={({ match, history }) => {
+                        localStorage.setItem('token', match.params.token);
+                        return <Redirect to="/shorten" />
+                    }} />
                     <Route>
                         <RedirectComponent />
                     </Route>
                 </Switch>
-            </LoggedInContext.Provider>
-        </div>
+            </div>
+        </MantineProvider>
     )
 }
 
