cec250cb5e082c8693091f48c762e089f3c2f4ff
Francisco Manríquez Novoa <49853152+chopan050@users.noreply.github.com>
2023-11-09 15:06:15 -0300

    Fixed bug where non-dFunkt users could not shorten links (#51)
    * Fixed bug where non-dFunkt users could not shorten links because they were not in dFunkt DB
    
    * Fixed lint warnings (I dislike you, TypeScript)
    
    * Fixed more lint warnings
    
    * Goddamn linter
    
    * Fixed all the lints for middlewares.ts
    
    * Definitely fixed all the lints for middlewares.ts
    
    * Addressed requested changes
    
    * Check if dfunktError is AxiosError at authorizePls

--- middlewares.ts
+++ middlewares.ts
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios, { AxiosResponse, AxiosError } from "axios";
 import { validationResult } from "express-validator";
 import httpContext from "express-http-context";
 import { configuration } from "./configuration";
 import { NextFunction, Request, Response } from "express";
 import { canCreateCustomLinks, getContext, hasMandate, isAdmin } from "./utils";
-import { CurrentMandate } from "./types";
+import { CurrentMandate, User } from "./types";
 
 export const validationCheck = (
     req: Request,
@@ -23,12 +23,13 @@ export const authorizePls = async (
     res: Response,
     next: NextFunction
 ) => {
-    const authorizationHeader = req.headers.authorization;
-    let token;
+    const authorizationHeader: string | undefined = req.headers.authorization;
+    let token: string | undefined;
+
     if (authorizationHeader) {
         token = authorizationHeader.split(" ")[1];
     } else if (req.query.token) {
-        token = req.query.token;
+        token = req.query.token as string;
     }
 
     if (!token || token.length === 0) {
@@ -36,34 +37,55 @@ export const authorizePls = async (
         return;
     }
 
+    const loginUrl = `${configuration.LOGIN_API_URL}/verify/${token}.json?api_key=${configuration.LOGIN_API_KEY}`;
+    let loginResponse: AxiosResponse;
     try {
-        const response = await axios.get(
-            `${configuration.LOGIN_API_URL}/verify/${token}.json?api_key=${configuration.LOGIN_API_KEY}`
-        );
-        if (response.status !== 200) {
-            res.sendStatus(401);
-            return;
+        loginResponse = await axios.get(loginUrl);
+    } catch (err) {
+        const loginError = err as AxiosError;
+        if (loginError.response) {
+            const { status, data } = loginError.response;
+            res.status(status).send(data);
+        } else {
+            res.status(502).send(
+                "Bad Gateway - No response from the login API."
+            );
         }
+        return;
+    }
 
-        const user = response.data;
+    const user: User = loginResponse.data;
 
-        const plsResponse = await axios.get(
-            `${configuration.PLS_API_URL}/user/${user.user}/pico`
-        );
+    const plsUrl = `${configuration.PLS_API_URL}/user/${user.user}/pico`;
+    let plsResponse: AxiosResponse;
+    try {
+        plsResponse = await axios.get(plsUrl);
+    } catch (err) {
+        const plsError = err as AxiosError;
+        if (plsError.response) {
+            const { status, data } = plsError.response;
+            res.status(status).send(data);
+        } else {
+            res.status(502).send("Bad Gateway - No response from the PLS API.");
+        }
+        return;
+    }
+
+    let mandates: { title: string; identifier: string }[] = [];
+    let groups: { name: string; identifier: string }[] = [];
+    try {
         // Fetch user's mandates from dfunkt
         // TODO: Cache this
-        const result: CurrentMandate[] = (
-            await axios.get(
-                `https://dfunkt.datasektionen.se/api/user/kthid/${user.user}/current`
-            )
-        ).data.mandates;
-        const mandates = result
+        const dfunktUrl = `https://dfunkt.datasektionen.se/api/user/kthid/${user.user}/current`;
+        const dfunktResponse = await axios.get(dfunktUrl);
+        const result: CurrentMandate[] = dfunktResponse.data.mandates;
+        mandates = result
             // Only save title and identifier
             .map((m) => ({
                 title: m.Role.title,
                 identifier: m.Role.identifier,
             }));
-        const groups = result
+        groups = result
             .map((m) => ({
                 name: m.Role.Group.name,
                 identifier: m.Role.Group.identifier,
@@ -72,19 +94,38 @@ export const authorizePls = async (
                 (v, i, self) =>
                     i === self.findIndex((t) => t.identifier === v.identifier)
             );
-
-        httpContext.set("user", {
-            ...user,
-            pls: plsResponse.data,
-            mandates,
-            groups,
-        });
-
-        next();
     } catch (err) {
-        res.sendStatus(500);
-        return;
+        const dfunktError = err as AxiosError;
+        if (!dfunktError.isAxiosError) {
+            res.status(500).send(
+                "Internal Server Error - " + dfunktError.message
+            );
+            return;
+        }
+        if (dfunktError.response) {
+            const { status, data } = dfunktError.response;
+            // If status is 404, this user is simply not a dFunkt and the page
+            // should proceed normally. Otherwise, return an error.
+            if (status != 404) {
+                res.status(status).send(data);
+                return;
+            }
+        } else {
+            res.status(502).send(
+                "Bad Gateway - No response from the dFunkt API."
+            );
+            return;
+        }
     }
+
+    httpContext.set("user", {
+        ...user,
+        pls: plsResponse.data,
+        mandates,
+        groups,
+    });
+
+    next();
 };
 
 export const desiredAuth = (
